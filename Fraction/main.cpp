fd793175e8e2f29c109e72330cab316ac8694def
miraxxel <forremains@yandex.ru>
2024-05-31 18:09:17 +0300

    fraction in progress

---
+++ Fraction/main.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+using namespace std;
+
+// дробь определяется как отношение количества взятых частей 
+// к общему числу разделенных частей
+class Fraction
+{
+	int numerator;
+	int denominator;
+
+public:
+	// геттеры
+	int get_numerator() const { return numerator; }
+	int get_denominator() const { return denominator; }
+
+	// сеттеры
+	void set_numerator(int numerator) { this->numerator = numerator; }
+	void set_denominator(int denominator) 
+	{ 
+		if (denominator != 0) 
+			this->denominator = denominator; 
+	}
+	
+	// конструктор
+	Fraction(int numerator, int denominator)
+	{
+		this->numerator = numerator;
+		this->denominator = denominator;
+		cout << "(TwoArg) Constructor: \t" << this << endl;
+	}
+	// деструктор
+	~Fraction()
+	{
+		cout << "Destructor: \t\t" << this << endl;
+	}
+
+	// перегрузка операторов
+	// не успеваю, сделаю позже
+
+	// методы
+	void print()const
+	{
+		cout << this << ": fraction = " << numerator << "/" << denominator << endl;
+	}
+};
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	Fraction A(1, 2);
+	A.print();
+}
