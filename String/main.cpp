82dad0d93f928b4d50f959f687d46721755d0c00
miraxxel <forremains@yandex.ru>
2024-06-22 22:56:17 +0300

    string DONE

--- String/main.cpp
+++ String/main.cpp
@@ -16,9 +16,14 @@ public:
 	{
 		return size;
 	}
+	// для неконстантного объекта
+	char* get_str()
+	{
+		return str;
+	}
 	//			Constructors:
 	// конструктор по умолчанию, который создает пустую строку размером 80 байт
-	String(int size = 80)
+	explicit String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
@@ -31,25 +36,59 @@ public:
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
+
+	String(const String& other)
+	{
+		// Deep Copy
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+
 	~String()
 	{
 		delete[] this->str;
 		cout << "Destructor:\t\t" << this << endl;
 	}
 
+	//			Operators:
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;
+
+		delete[] str;
+		this->size = other.size;
+		this->str = new char[size] {};
+
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+
+		return *this;
+	}
 	//			Methods:
 	void print()const
 	{
-		cout << "Size:\t" << size << endl;
+		cout << "Obj:\t" << this<<"\t";
+		cout << "Size:\t" << size << "\t";
+		cout << "Addr:\t" << &str << "\t";
 		cout << "Str:\t" << str << endl;
 	}
 };
 
-String operator+(const String& str1, const String& str2) 
+String operator+(const String& left, const String& right) 
 {
-	int newSize = str1.get_size() + str2.get_size(); 
-	char* newStr = new char[newSize + 1];
-	// пока не готово
+	/*int newSize = str1.get_size() + str2.get_size(); 
+	char* newStr = new char[newSize + 1];*/
+
+	// -1 т.к. в конце каждой строке есть конкатенирующий ноль, здесь у нас их 2
+	String buffer(left.get_size() + right.get_size() - 1);	// from 'int' to 'string'
+	for (int i = 0; i < left.get_size(); i++)
+		buffer.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	buffer.print();
+	return buffer;
 }
 
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -66,10 +105,14 @@ void main()
 
 	String str1 = "Hello";
 	String str2 = "World";
+
+	str1 = str1;
 	
+	//String str3 = str1 + str2; // CopyConstructor
+	String str3;
+	str3 = str1 + str2;	// CopyAssignment
+	//str3.print();
 	cout << str1 << endl;
 	cout << str2 << endl;
-	
-	String str3 = str1 + str2;
-	cout << str3 << endl;
+	cout << str3 << endl;	// HelloWorld
 }
